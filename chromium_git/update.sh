171691048218ea2282308182c7bf86b5ef8558fb
Ray C Horn <raychorn@gmail.com>
2020-09-27 18:01:30 -0600

    Added more to .gitignore.

--- chromium_git/update.sh
+++ chromium_git/update.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python ../automate/automate-git.py --download-dir="/home/raychorn/@1/cef/chromium_git" --depot-tools-dir="/home/raychorn/@1/cef/depot_tools" --no-distrib --no-build
+sudo python ../automate/automate-git.py --download-dir="/home/raychorn/@1/cef/chromium_git" --depot-tools-dir="/home/raychorn/@1/cef/depot_tools" --no-distrib --no-build
 
