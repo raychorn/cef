9a3c31837170756a9152e60a872779edcc0a71db
Ray C Horn <raychorn@gmail.com>
2020-09-27 18:35:06 -0600

    Not working. For now.

--- chromium_git/update.sh
+++ chromium_git/update.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+export PATH=/home/raychorn/@1/cef/depot_tools:$PATH
 sudo python ../automate/automate-git.py --download-dir="/home/raychorn/@1/cef/chromium_git" --depot-tools-dir="/home/raychorn/@1/cef/depot_tools" --no-distrib --no-build
 
